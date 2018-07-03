325fa6433ca9fae9d3618be65e66281cd240993f
tutkuince <tutku.ince@outlook.com>
2018-07-03 13:23:09 +0300

    add comments

--- src/io/spring/part05/MainApp.java
+++ src/io/spring/part05/MainApp.java
@@ -6,16 +6,16 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class MainApp {
 	public static void main(String[] args) {
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-		
-		DBSingleton dbSingleton = context.getBean("dbSingleton", DBSingleton.class);
-		Connection conn = dbSingleton.getConnection();
-		
-		if(conn != null)
-			System.out.println("Connection is not null");
-		else
-			System.out.println("Connection is null");
-		
-		context.close();
+//		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+//		
+//		DBSingleton dbSingleton = context.getBean("dbSingleton", DBSingleton.class);
+//		Connection conn = dbSingleton.getConnection();
+//		
+//		if(conn != null)
+//			System.out.println("Connection is not null");
+//		else
+//			System.out.println("Connection is null");
+//		
+//		context.close();
 	}
 }
